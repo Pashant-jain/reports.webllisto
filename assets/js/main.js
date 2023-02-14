ae711a3baaf047376474f054d319d7c286d055db
prashant <prashant.jain@webllisto.com>
2023-02-14 16:32:59 +0530

    add: created timesheet page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-$(".slidebarCheck").click(function(){
-    $(".dashboard_wrp").toggleClass("closed_slidebar");
-  });
-
-
-
-  //new one 
-
   $(".sidebar_toggler").click(function(){
     $(".sidebar").toggleClass("expand_sidebar");
     $(".sidebar_toggler").toggleClass("rotate-180");
