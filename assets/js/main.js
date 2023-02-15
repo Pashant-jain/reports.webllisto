36f6e0f4dcb4887d2c375da957dcd5f1a872cd27
prashant <prashant.jain@webllisto.com>
2023-02-15 16:22:28 +0530

    project list table ui design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,19 @@
-  $(".sidebar_toggler").click(function(){
-    $(".sidebar").toggleClass("expand_sidebar");
-    $(".sidebar_toggler").toggleClass("rotate-180");
-    $(".contant").toggleClass("expand_contant");
-  });
-
-
-
-  $(document).ready(function () {
-    $('#example').DataTable({
-        lengthMenu: [
-            [10, 25, 50, -1],
-            [10, 25, 50, 'All'],
-        ],
-        pagingType: 'simple',
-        responsive: true
-    });
+$(".sidebar_toggler").click(function () {
+  $(".sidebar").toggleClass("expand_sidebar");
+  $(".sidebar_toggler").toggleClass("rotate-180");
+  $(".contant").toggleClass("expand_contant");
 });
 
-// $(document).ready(function () {
-//   $('#example').DataTable({
-    
-//   });
-// });
+$(document).ready(function () {
+  $("#list_table").DataTable({
+    lengthMenu: [
+      [10, 25, 50, -1],
+      [10, 25, 50, "All"],
+    ],
+    pagingType: "simple",
+    responsive: true,
+  });
+  $(".report_list_table").DataTable({
+    searching: false,
+  });
+});
