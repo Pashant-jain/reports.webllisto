a78e9dffa0c410fee3e0ee3874b679f4adbcaaff
shubhamwebllisto <shubham.sahu@webllisto.com>
2023-03-03 04:13:11 +0530

    landing page design

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-jQuery(document).ready(function($){
     $(document).ready(function(){
-        $('.slider').bxSlider();
+            $('.bxslider').bxSlider({
+            controls:true,
+              captions: true,
+			  stopAutoOnClick: true,
+			  pager: false,
+			  minSlides:3,
+			  maxSlides:3,
+			  moveSlides:1,
+			  slideMargin:50, 
+			  infiniteLoop:false,
+			  slideWidth: 400,
+              autoControls:true,     
+            });
+         
         });
-    });
+       
